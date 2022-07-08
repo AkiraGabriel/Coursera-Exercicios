9f54d8cfbd7afd4653d28676905189b0536dc1b1
Gabriel Akira <gabriel.akira2008@hotmail.com>
2022-07-08 13:43:57 -0300

    Add files via upload

---
+++ Semana 2/Ex1.py
@@ -0,0 +1,5 @@
+l = int(input("Digite o valor correspondente ao lado de um quadrado: "))
+p = l * 4
+a = l**2 
+
+print("perímetro: {} - área: {}".format(p, a))
---
+++ Semana 2/Ex2.py
@@ -0,0 +1,7 @@
+cliente_Nome = input("Digite o nome do cliente: ")
+vencimento_Dia = input("Digite o dia de vencimento: ")
+vencimento_Mes = input("Digite o mês de vencimento: ")
+fatura_Valor = (input("Digite da fatura: "))
+
+print("Olá, {}".format(cliente_Nome))
+print("A sua fatura com o vencimento em "+vencimento_Dia+" de "+vencimento_Mes+" no valor de R$ "+fatura_Valor+" está fechada.")
