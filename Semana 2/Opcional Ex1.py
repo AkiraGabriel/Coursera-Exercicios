d851e4c90db4c69819f01f2a3edede7bdbebb74b
Gabriel Akira <gabriel.akira2008@hotmail.com>
2022-07-08 13:43:35 -0300

    Add files via upload

---
+++ Semana 2/Opcional Ex1.py
@@ -0,0 +1,7 @@
+NomeCliente = input("Digite o nome do cliente: ")
+DiaVencimento = input("DIgite o dia de vencimento: ")
+MesVencimento = input("DIgite o mês de vencimento: ")
+ValorFatura = input("Digite o valor de fatura: ") 
+
+print("Olá,", NomeCliente)
+print("A sua fatura com vencimento em",DiaVencimento,"de",MesVencimento,"no valor de R$",ValorFatura,"está fechada.")
