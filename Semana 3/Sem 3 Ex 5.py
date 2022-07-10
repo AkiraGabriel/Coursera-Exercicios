206cf6f0eb507f6c12cc2efaccedc19f64b7b122
Gabriel Akira <gabriel.akira2008@hotmail.com>
2022-07-10 02:03:39 -0300

    a

---
+++ Semana 3/Sem 3 Ex 5.py
@@ -0,0 +1,7 @@
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
+c = int(input("Digite outro número: "))
+if a<b<c:
+    print("crescente")
+else:
+    print("não está em ordem crescente")
